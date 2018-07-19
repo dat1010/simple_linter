b2b4254cec9f684648d0ebb8b212ae0119ce86ec
David Tanner <davidtanner@localhost.localdomain>
2018-07-19 10:16:36 -0400

    initial commit

---
+++ LinterService.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace LinterService.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ LinterService/LinterService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Linter.Service
+{
+    public class LinterService
+    {
+			public bool IsLinter()
+			{
+				throw new NotImplementedException("This is not yet working");
+			}
+    }
+}
